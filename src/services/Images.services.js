2ba73b45d72f7155eccf7274aa4a18ebbf0e89c4
agustina.martinez <agustina.martinez@globant.com>
2021-02-09 13:59:57 -0300

    add environment variable

--- src/services/Images.services.js
+++ src/services/Images.services.js
@@ -1,5 +1,5 @@
 export const getFetchData = async () => {
-    const res = await fetch(`https://api.unsplash.com/photos/?page=10&per_page=20&order_by=latest&client_id=ZJIb9mKmajs7KbwSRFk3afyEvZm52fxN2tnYVAKYkNA`);
+    const res = await fetch(`https://api.unsplash.com/photos/?page=10&per_page=20&order_by=latest&client_id=${process.env.REACT_APP_CLIENT_ID}`);
     const data = await res.json();
     return data;
 }
